9e0fb20ffe439a288b786e5fb451fe896380d7ee
nyh <2830481694@qq.com>
2025-08-11 11:01:54 +0800

    :sparkles: feat(packages/tools): 新增随机数
    新增随机数函数可以根据传入的参数和结束范围来生成随机数

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,3 +1,13 @@
 export function sum<T>(a: T, b: T): T {
     return (a as any) + (b as any);
 }
+
+/**
+ * 生成随机数
+ * @param start 开始范围
+ * @param end 结束范围
+ * @returns 随机数
+ */
+export function random(start: number, end: number): number {
+    return Math.floor(Math.random() * (end - start + 1)) + start;
+}
